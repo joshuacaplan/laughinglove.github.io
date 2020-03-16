80b0414d4669a0dcf9bfad06d3811c30983e5f4f
LaughingLove <jokelovehate@gmail.com>
2020-03-16 00:54:37 -0400

    Init
    Added the basic index.html stuff for the landing page

---
+++ js/menu.js
@@ -0,0 +1,19 @@
+// window.onload = function () {
+//     console.log("WTFFFF");
+//     var burgerNav = document.getElementsByClassName("fa-bars");
+//     burgerNav.onclick = function () {
+//         console.log("onclick");
+//         var navMain = document.getElementsByClassName("navmain");
+//         navMain.classList.toggle("open");
+//     };
+//     // burgerNav.onclick(function () {
+//     //     console.log("Hello");
+//     // })
+// };
+
+$(document).ready(function () {
+    $(".burgerbutton").on('click', function () {
+        console.log("hello");
+        $(".navmain").toggleClass("open");
+    })
+})
